0c35f61416cd74ef3f9cec434a2f6d2e0a08f736
Raúl Pedraza <raul@corpora360.com>
2018-02-23 22:52:40 +0100

    Setup presenter.

--- MovieApp/Presenter/MoviesPresenter.swift
+++ MovieApp/Presenter/MoviesPresenter.swift
@@ -1,5 +1,29 @@
 import Foundation
 
 class MoviesPresenter {
+    var interactor: MoviesInteractorProtocol!
+    var routing: MoviesRoutingProtocol!
+    weak var view: MoviesViewProtocol!
+    //MARK: MoviesPresenterProtocol
+    func execute() {
+        interactor.execute(completionHandler: {
+            self.view.reloadData()
+        })
+    }
     
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRows(at section: Int) -> Int {
+        return interactor.allMovies().count
+    }
+    
+    func selectRow(at indexPath: IndexPath) {
+        let superHero = interactor.movie(at: indexPath)
+    }
+    
+    func superHero(at indexPath: IndexPath) -> Movie {
+        return interactor.movie(at: indexPath)
+    }
 }
