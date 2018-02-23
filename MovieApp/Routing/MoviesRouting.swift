e423c747c58b3681cb482ee0122e6e2e2a11a43b
Raúl Pedraza <raul@corpora360.com>
2018-02-23 23:37:03 +0100

    Setup movies routing.

--- MovieApp/Routing/MoviesRouting.swift
+++ MovieApp/Routing/MoviesRouting.swift
@@ -1,5 +1,22 @@
-import Foundation
+import UIKit
 
-class MoviesRouting {
+class MoviesRouting: MoviesRoutingProtocol {
+    private let navigationController: MoviesNavigationController!
+    private let viewController: MoviesViewController!
     
+    init(navigationController: MoviesNavigationController, viewController: MoviesViewController) {
+        self.navigationController = navigationController
+        self.viewController = viewController
+    }
+    
+    func setup() {
+        let superHeroRepository = MoviesRepository()
+        let interactor = MoviesInteractor(repository: superHeroRepository)
+        let presenter = MoviesPresenter()
+        presenter.interactor = interactor
+        presenter.routing = self
+        presenter.view = viewController
+        viewController.presenter = presenter
+    }
 }
+
