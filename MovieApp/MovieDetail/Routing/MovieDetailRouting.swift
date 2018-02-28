b4b1e7795ff4527d24972efb2021598c99a16946
Raúl Pedraza <raul@corpora360.com>
2018-02-28 09:19:11 +0100

    Setup Movie detail routing VIPER module.

---
+++ MovieApp/MovieDetail/Routing/MovieDetailRouting.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class MovieDetailRouting: MovieDetailRoutingProtocol {
+    private let viewController: MovieDetailViewController!
+    
+    init(viewController: MovieDetailViewController) {
+        self.viewController = viewController
+    }
+    
+    func setup() {
+        let interactor = MovieDetailInteractor()
+        let presenter = MovieDetailPresenter()
+        presenter.interactor = interactor
+        presenter.routing = self
+        presenter.view = viewController
+        viewController.presenter = presenter
+    }
+}
