691f9435a6e922b315605f898c1dd3ff7c8a652f
Raúl Pedraza <raul@corpora360.com>
2018-03-04 23:23:46 +0100

    Favorite module Realm model.

--- Persistence/FavoriteMovie.swift
+++ Persistence/FavoriteMovie.swift
@@ -2,5 +2,15 @@ import Foundation
 import RealmSwift
 
 class FavoriteMovie: Object {
+    @objc dynamic var id = 0
+    @objc dynamic var title = ""
+    @objc dynamic var imagePath = ""
     
+    func setup(with movie: Movie) -> FavoriteMovie {
+        let favoriteMovie = FavoriteMovie()
+        favoriteMovie.id = movie.id
+        favoriteMovie.title = movie.title
+        favoriteMovie.imagePath = movie.imagePath
+        return favoriteMovie
+    }
 }
