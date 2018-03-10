9267482782b217c86a7615dd97558c2aff966010
Raúl Pedraza <raul@corpora360.com>
2018-03-10 20:16:25 +0100

    Setup favorite movie storage.

--- MovieApp/Repository/FavoriteMovies/FavoriteMoviesStorage.swift
+++ MovieApp/Repository/FavoriteMovies/FavoriteMoviesStorage.swift
@@ -1,4 +1,5 @@
 import Foundation
+import RealmSwift
 
 class FavoriteMoviesStorage {
     private var favoriteMovies = [FavoriteMovie]()
@@ -9,5 +10,43 @@ class FavoriteMoviesStorage {
     
     var allFavoriteMovies: [FavoriteMovie] {
         return favoriteMovies
+    }
+    
+    func removeFavoriteMovie(at indexPath: IndexPath) {
+        do {
+            let realm = try Realm()
+            let favoriteMovieToCheck = favoriteMovies[indexPath.row]
+            if let favoriteMovie = favoriteMovie(with: favoriteMovieToCheck.id) {
+                try realm.write {
+                    realm.delete(favoriteMovie)
+                }
+            }
+        } catch let error {
+            print("\(error.localizedDescription)")
+        }
+        favoriteMovies.removeAll()
+    }
+    
+    func saveFavoriteMovie(movie: Movie) {
+        do {
+            let realm = try Realm()
+            let favoriteMovieToCheck = FavoriteMovie().setup(with: movie)
+            guard let _ = favoriteMovie(with: favoriteMovieToCheck.id) else {
+                try realm.write {
+                    realm.add(favoriteMovieToCheck)
+                }
+                return
+            }
+        } catch let error {
+            print("\(error.localizedDescription)")
+        }
+    }
+    
+    private func favoriteMovie(with id: Int) -> FavoriteMovie? {
+        let realm = try! Realm()
+        guard let favoriteMovie = realm.object(ofType: FavoriteMovie.self, forPrimaryKey: id) else {
+            return nil
+        }
+        return favoriteMovie
     }
 }
